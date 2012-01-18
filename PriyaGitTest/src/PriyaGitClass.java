c0fabd659274a3b436dcfba4c24ff7c20850b097
priya-marimuthu <dharshinipriya37@gmail.com>
2012-01-18 14:54:13 +0530

    Commit Success Print

--- PriyaGitTest/src/PriyaGitClass.java
+++ PriyaGitTest/src/PriyaGitClass.java
@@ -4,6 +4,7 @@ public class PriyaGitClass {
 	public static void main(String args[]){
 		System.out.println("It is working");
 		System.out.println("It is Not working");
+		System.out.println("Success..!");
 	}
 	
 }
